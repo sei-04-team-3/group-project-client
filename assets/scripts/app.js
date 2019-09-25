3905ef4129a1e0adf44ab5639b27553a2bdffcc3
dtersoff <daverboy21@gmail.com>
2019-09-25 10:59:16 -0400

    Add startup sign-in page
    Add script to app file to plug sign-in handlebar into main-content div
    on startup

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,8 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const signInFormTemplate = require('./templates/sign-in.handlebars')
 
 $(() => {
-  // your JS code goes here
+  $('.main-content').html(signInFormTemplate())
 })
