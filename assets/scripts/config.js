e1994b92dd4def0ef601ce3e280e28588f672d5a
dtersoff <daverboy21@gmail.com>
2019-09-26 16:23:39 -0400

    Add url for deployed server

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://floating-lowlands-13889.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
