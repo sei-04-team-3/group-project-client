f4367c0031d195effc1cf7d3f975c19bafde27e4
dtersoff <daverboy21@gmail.com>
2019-09-27 09:28:16 -0400

    Add message update display feature
    When a message is updated, it displays that it's been updated, and the
    time at which it was updated

--- assets/scripts/templates/helpers/pretty-timestamps.js
+++ assets/scripts/templates/helpers/pretty-timestamps.js
@@ -1,9 +1,14 @@
 'use strict'
 
-const moment = require('moment'
-)
-const prettyTime = date => {
-  return moment(date).fromNow()
+const moment = require('moment')
+const isEdited = require('./is-edited.js')
+
+const prettyTime = message => {
+  if (isEdited(message)) {
+    return 'posted ' + moment(message.createdAt).fromNow()
+  } else {
+    return 'updated ' + moment(message.updatedAt).fromNow()
+  }
 }
 
 module.exports = prettyTime
